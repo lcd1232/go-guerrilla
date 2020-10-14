aa3eeff4df060ead4dc4a5517519a0662b54d34a
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2020-10-14 15:57:42 +0300

    Update server.go

--- server.go
+++ server.go
@@ -365,11 +365,16 @@ func (s *server) handleClient(client *client) {
 	defer client.closeConn()
 	sc := s.configStore.Load().(ServerConfig)
 	s.log().Infof("Handle client [%s], id: %d", client.RemoteIP, client.ID)
-
+	
 	// Initial greeting
-	greeting := fmt.Sprintf("220 %s SMTP Guerrilla(%s) #%d (%d) %s",
-		sc.Hostname, Version, client.ID,
-		s.clientPool.GetActiveClientsCount(), time.Now().Format(time.RFC3339))
+	var greeting string
+	if sc.Greeting != "" {
+		greeting = fmt.Sprintf("220 %s %s", sc.Hostname, sc.Greeting)
+	} else {
+		greeting := fmt.Sprintf("220 %s SMTP Guerrilla(%s) #%d (%d) %s",
+			sc.Hostname, Version, client.ID,
+			s.clientPool.GetActiveClientsCount(), time.Now().Format(time.RFC3339))
+	}
 
 	helo := fmt.Sprintf("250 %s Hello", sc.Hostname)
 	// ehlo is a multi-line reply and need additional \r\n at the end
